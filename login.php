4ed6c84d6a4b41da64a8534541ef2c6a1da960eb
pcsaini <premchandsaini779@gmail.com>
2016-09-24 19:10:27 +0530

    create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<div class="form-content">
+    <div class="form">
+        <h2>Login to your account</h2>
+        <form action="" method="post">
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <button>Login</button>
+        </form>
+        <div class="cta"><a href="#">Forgot your password?</a></div>
+    </div>
+</div>
+</body>
+</html>
