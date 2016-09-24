b49cf252c0efa987c547d156d1cbdcd3a9278cbd
pcsaini <premchandsaini779@gmail.com>
2016-09-24 19:15:58 +0530

    add logo

--- login.php
+++ login.php
@@ -8,9 +8,12 @@
 </head>
 
 <body>
+<div class="login-logo">
+    <img src="image/logo.png">
+</div>
 <div class="form-content">
     <div class="form">
-        <h2>Login to your account</h2>
+        <h2>Login</h2>
         <form action="" method="post">
             <input type="text" placeholder="Username"/>
             <input type="password" placeholder="Password"/>
