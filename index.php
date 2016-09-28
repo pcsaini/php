95a81c1768ac5bc0d84cbc9efa6b58bb9e782ada
pcsaini <premchandsaini779@gmail.com>
2016-09-28 16:40:44 +0530

    activities

--- index.php
+++ index.php
@@ -30,6 +30,37 @@
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="about">
+            <h2 class="text-center">About</h2>
+            <p class="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div class="activities">
+            <h2 class="text-center">Last Activities</h2>
+            <div class="row">
+                <div class="col-xs-12 col-md-3 col-sm-12 col-lg-3">
+                    <div class="quarter">
+                        <i class="fa fa-book fa-3x fa-align-left"></i><span class="number">25</span> <br/><br/><span>Most issued Books</span>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-3 col-sm-12 col-lg-3">
+                    <div class="quarter">
+                        <i class="fa fa-book fa-3x fa-align-left"></i><span class="number">25</span> <br/><br/><span>Most issued Books</span>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-3 col-sm-12 col-lg-3">
+                    <div class="quarter">
+                        <i class="fa fa-book fa-3x fa-align-left"></i><span class="number">25</span> <br><br/><span>Most issued Books</span>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-3 col-sm-12 col-lg-3">
+                    <div class="quarter">
+                        <i class="fa fa-book fa-3x fa-align-left"></i><span class="number">25</span> <br><br/><span>Most issued Books</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
