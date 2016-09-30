0169ed6224461580d98016f348f6a6b196949a26
pcsaini <premchandsaini779@gmail.com>
2016-09-30 11:14:27 +0530

    set header

---
+++ dashboard/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="assets/image/logo.ico">
+    <link rel="shortcut icon" type="image/x-icon" href=assets/image/logo.ico">
+    <title>Dashboard - Home</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+</head>
+<body>
+<?php
+    include_once "navbar.php";
+    //include_once "header.php";
+    //include_once "footer.php";
+?>
+
+
+
+    <script src="assets/js/jquery-3.1.1.min.js"></script>
+    <script src='assets/js/bootstrap.min.js'></script>
+</body>
+</html>
---
+++ dashboard/navbar.php
@@ -0,0 +1,102 @@
+<nav class="navbar-default navbar-static-top" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+
+        <h1> <a class="navbar-brand" href="index.html">Minimal</a></h1>
+    </div>
+        <div class="border-bottom">
+            <div class="full-left">
+                <section class="full-top">
+                    <button id="toggle"><i class="fa fa-arrows-alt"></i></button>
+                </section>
+                <form class=" navbar-left-right">
+                    <input type="text"  value="Search..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search...';}">
+                    <input type="submit" value="" class="fa fa-search">
+                </form>
+                <div class="clearfix"> </div>
+            </div>
+            <div class="drop-men" >
+                <ul class=" nav_1">
+                    <li class="dropdown at-drop">
+                        <a href="#" class="dropdown-toggle dropdown-at " data-toggle="dropdown"><i class="fa fa-globe"></i> <span class="number">5</span></a>
+                        <ul class="dropdown-menu menu1 " role="menu">
+                            <li><a href="#">
+
+                                    <div class="user-new">
+                                        <p>New user registered</p>
+                                        <span>40 seconds ago</span>
+                                    </div>
+                                    <div class="user-new-left">
+
+                                        <i class="fa fa-user-plus"></i>
+                                    </div>
+                                    <div class="clearfix"> </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="user-new">
+                                        <p>Someone special liked this</p>
+                                        <span>3 minutes ago</span>
+                                    </div>
+                                    <div class="user-new-left">
+
+                                        <i class="fa fa-heart"></i>
+                                    </div>
+                                    <div class="clearfix"> </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="user-new">
+                                        <p>John cancelled the event</p>
+                                        <span>4 hours ago</span>
+                                    </div>
+                                    <div class="user-new-left">
+
+                                        <i class="fa fa-times"></i>
+                                    </div>
+                                    <div class="clearfix"> </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="user-new">
+                                        <p>The server is status is stable</p>
+                                        <span>yesterday at 08:30am</span>
+                                    </div>
+                                    <div class="user-new-left">
+
+                                        <i class="fa fa-info"></i>
+                                    </div>
+                                    <div class="clearfix"> </div>
+                                </a></li>
+                            <li><a href="#">
+                                    <div class="user-new">
+                                        <p>New comments waiting approval</p>
+                                        <span>Last Week</span>
+                                    </div>
+                                    <div class="user-new-left">
+
+                                        <i class="fa fa-rss"></i>
+                                    </div>
+                                    <div class="clearfix"> </div>
+                                </a></li>
+                            <li><a href="#" class="view">View all messages</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle dropdown-at" data-toggle="dropdown"><span class=" name-caret">Rackham<i class="caret"></i></span><img src="images/wo.jpg"></a>
+                        <ul class="dropdown-menu " role="menu">
+                            <li><a href="profile.html"><i class="fa fa-user"></i>Edit Profile</a></li>
+                            <li><a href="inbox.html"><i class="fa fa-envelope"></i>Inbox</a></li>
+                            <li><a href="calendar.html"><i class="fa fa-calendar"></i>Calender</a></li>
+                            <li><a href="inbox.html"><i class="fa fa-clipboard"></i>Tasks</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="clearfix">
+
+            </div>
+</nav>
