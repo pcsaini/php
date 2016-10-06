cd20c5918963b9bd216b644544f597bd9726a8c3
pcsaini <premchandsaini779@gmail.com>
2016-10-07 01:41:17 +0530

    add unreserved files

---
+++ dashboard/sidebar.php
@@ -0,0 +1,74 @@
+<div class="side-menu sidebar-inverse">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="side-menu-container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <div class="fa fa-paper-plane icon"></div>
+                    <div class="title">Library</div>
+                </a>
+                <button type="button" class="navbar-right-expand-toggle pull-right visible-xs">
+                    <i class="fa fa-times icon"></i>
+                </button>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="index.php">
+                       <span class="icon fa fa-tachometer"></span>
+                       <span class="title">Dashboard</span>
+                    </a>
+                </li>
+                <li class="panel panel-default dropdown">
+                    <a data-toggle="collapse" href="#dropdown-element">
+                        <span class="icon fa fa-desktop"></span>
+                        <span class="title">LINK - 1</span>
+                    </a>
+                    <!--Dropdown -->
+                    <div id="dropdown-element" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#">Sub-link 1</a></li>
+                                <li><a href="#">Sub-link 2</a></li>
+                                <li><a href="#">Sub-link 3</a></li>
+                                <li><a href="#">Sub-link 4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="panel panel-default dropdown">
+                    <a data-toggle="collapse" href="#dropdown-element1">
+                        <span class="icon fa fa-desktop"></span>
+                        <span class="title">LINK - 2</span>
+                    </a>
+                    <!--Dropdown -->
+                    <div id="dropdown-element1" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#">Sub-link 1</a></li>
+                                <li><a href="#">Sub-link 2</a></li>
+                                <li><a href="#">Sub-link 3</a></li>
+                                <li><a href="#">Sub-link 4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="panel panel-default dropdown">
+                    <a data-toggle="collapse" href="#dropdown-element2">
+                        <span class="icon fa fa-desktop"></span>
+                        <span class="title">LINK - 3</span>
+                    </a>
+                    <!--Dropdown -->
+                    <div id="dropdown-element2" class="panel-collapse collapse">
+                        <div class="panel-body">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#">Sub-link 1</a></li>
+                                <li><a href="#">Sub-link 2</a></li>
+                                <li><a href="#">Sub-link 3</a></li>
+                                <li><a href="#">Sub-link 4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
