2fb11dbab9e01f99a202f09a01f0936d6bda040d
Anjali Kumari <anjali3112soni@gmail.com>
2016-11-13 09:57:35 +0530

    updated connect.php

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,6 +1,7 @@
 <?php
 $connect_error = 'sorry problems in connection';
-mysql_connect('localhost','root','vishraman') or die($connect_error);
+
+mysql_connect('localhost','root','') or die($connect_error);
 mysql_select_db('library') or die($connect_error);
 
 ?>
