41fb987e309361b6af2dc4db3106812dc5c4d56a
vishal raman <visraman26@gmail.com>
2016-11-13 09:44:57 +0530

    connect.php comment

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,7 +1,15 @@
 <?php
-$connect_error = 'sorry problems in connection';
-mysql_connect('localhost','root','') or die($connect_error);
-mysql_select_db('library') or die($connect_error);
+
+
+
+
+
+
+
+
+/*$connect_error = 'sorry problems in connection';
+mysql_connect('localhost','root','vishraman') or die($connect_error);
+mysql_select_db('library') or die($connect_error);*/
 
 ?>
 
