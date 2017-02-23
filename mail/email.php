d68d7f4fb23f449a6122c3adabdcf2d398c5d0c1
pcsaini <premchandsaini779@gmail.com>
2017-02-23 20:09:04 +0530

    Mail

---
+++ mail/email.php
@@ -0,0 +1,146 @@
+<?php
+
+$mailmessage = '<table width="100%" border="0" cellspacing="0" cellpadding="0" style="min-width: 320px;"><tr><td align="center" bgcolor="#eff3f8">
+
+
+<!--[if gte mso 10]>
+<table width="680" border="0" cellspacing="0" cellpadding="0">
+<tr><td>
+<![endif]-->
+
+<table border="0" cellspacing="0" cellpadding="0" class="table_width_100" width="100%" style="max-width: 680px; min-width: 300px;">
+    <tr><td>
+	<!-- padding -->
+	</td></tr>
+	<!--header -->
+	<tr><td align="center" bgcolor="#ffffff">
+		<!-- padding -->
+		<table width="90%" border="0" cellspacing="0" cellpadding="0"><div style="height: 30px; line-height: 30px; font-size: 10px;"></div>
+			<tr><td align="center">
+			    		<a href="#" target="_blank" style="color: #596167; font-family: Arial, Helvetica, sans-serif; float:left; width:100%; padding:20px;text-align:center; font-size: 13px;">
+									<font face="Arial, Helvetica, sans-seri; font-size: 13px;" size="3" color="#596167">
+									<img src="http://dev.yogabar.sg/web/images/img_logo.png" width="120" alt="Metronic" border="0"  /></font></a>
+					</td>
+					<td align="right">
+				<!--[endif]--><!-- 
+
+			</td>
+			</tr>
+		</table>
+		<!-- padding --><div style="height: 50px; line-height: 50px; font-size: 10px;"></div>
+	</td></tr>
+	<!--header END-->
+
+	<!--content 1 -->
+	<tr><td align="center" bgcolor="#fbfcfd">
+		<table width="90%" border="0" cellspacing="0" cellpadding="0">
+			<tr><td align="center">
+				<!-- padding --><div style="height: 60px; line-height: 60px; font-size: 10px;"></div>
+				<div style="line-height: 44px;">
+					<font face="Arial, Helvetica, sans-serif" size="5" color="#57697e" style="font-size: 34px;">
+					<span style="font-family: Arial, Helvetica, sans-serif; font-size: 34px; color: #57697e;">
+						Awesome User Expierence
+					</span></font>
+				</div>
+				<!-- padding --><div style="height: 40px; line-height: 40px; font-size: 10px;"></div>
+			</td></tr>
+			<tr><td align="center">
+				<div style="line-height: 24px;">
+					<font face="Arial, Helvetica, sans-serif" size="4" color="#57697e" style="font-size: 15px;">
+					<span style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; color: #57697e;">
+						Lorem ipsum dolor sit amet consectetuer sed<br> diam nonumy nibh elit dolore.
+					</span></font>
+				</div>
+				<!-- padding --><div style="height: 40px; line-height: 40px; font-size: 10px;"></div>
+			</td></tr>
+			<tr><td align="center">
+				<div style="line-height: 24px;">
+					<a href="#" target="_blank" class="btn btn-danger block-center">
+					    click
+					</a>
+				</div>
+				<!-- padding --><div style="height: 60px; line-height: 60px; font-size: 10px;"></div>
+			</td></tr>
+		</table>		
+	</td></tr>
+	<!--content 1 END-->
+
+
+	<!--footer -->
+	<tr><td class="iage_footer" align="center" bgcolor="#ffffff">
+
+		
+		<table width="100%" border="0" cellspacing="0" cellpadding="0">
+			<tr><td align="center" style="padding:20px;flaot:left;width:100%; text-align:center;">
+				<font face="Arial, Helvetica, sans-serif" size="3" color="#96a5b5" style="font-size: 13px;">
+				<span style="font-family: Arial, Helvetica, sans-serif; font-size: 13px; color: #96a5b5;">
+					2015 © Metronic. ALL Rights Reserved.
+				</span></font>				
+			</td></tr>			
+		</table>
+		
+
+	</td></tr>
+	<!--footer END-->
+	<tr><td>
+
+	</td></tr>
+</table>
+<!--[if gte mso 10]>
+</td></tr>
+</table>
+<![endif]-->
+ 
+</td></tr>
+</table>
+';
+send_mail("premchandsaini81@gmail.com", "subject", $mailmessage);	
+
+
+
+function send_mail($to,$subject,$body){
+
+	require('PHPMailer/PHPMailerAutoload.php');
+	$mail = new PHPMailer;
+	$mail->isSMTP();  
+	$mail->SMTPOptions = array(
+	    'ssl' => array(
+	        'verify_peer' => false,
+	        'verify_peer_name' => false,
+	        'allow_self_signed' => true
+	    )
+	);                                    // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'premchandsaini779@gmail.com';                   // SMTP username
+	$mail->Password = 'Pcsaini@779';               // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+	$mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
+	$mail->setFrom('premchandsaini779@gmail.com', 'Prem Chand Saini');     //Set who the message is to be sent from
+	$mail->addReplyTo('premchandsaini779@gmail.com', 'Prem Chand Saini');  //Set an alternative reply-to address
+	//$mail->addAddress('josh@example.net', 'Josh Adams');  // Add a recipient
+	$mail->addAddress($to);               // Name is optional
+	//$mail->addCC('cc@example.com');
+	//$mail->addBCC('bcc@example.com');
+	$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+	//$mail->addAttachment('/usr/labnol/file.doc');         // Add attachments
+	//$mail->addAttachment('/images/image.jpg', 'new.jpg'); // Optional name
+	$mail->isHTML(true);                                  // Set email format to HTML
+
+	$mail->Subject = $subject;
+	$mail->Body    = $body;
+	$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+	//Read an HTML message body from an external file, convert referenced images to embedded,
+	//convert HTML into a basic plain-text alternative body
+	//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+
+	if(!$mail->send()) {
+	   echo 'Message could not be sent.';
+	   echo 'Mailer Error: ' . $mail->ErrorInfo;
+	   exit;
+	}
+
+	echo 'Message has been sent';
+}
+?>
---
+++ mail/index.php
@@ -0,0 +1,4 @@
+<?php
+include('email.php');
+mail('libraryapp779@gmail.com', 'test', 'message');
+?>
